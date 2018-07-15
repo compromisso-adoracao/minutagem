ef02d72f1b365193e54fc892d7c27641ff799521
Vinicius Brasil <marcosbrasilaraujo@gmail.com>
2018-07-14 22:11:38 -0300

    Create Minutagem module

---
+++ src/minutagem.js
@@ -0,0 +1,20 @@
+const Minutagem = {
+  timeSignatures: [
+    { formula: "2x4",  tempoCount: 2 },
+    { formula: "3x4",  tempoCount: 3 },
+    { formula: "4x4",  tempoCount: 4 },
+    { formula: "6x4",  tempoCount: 6 },
+    { formula: "3x8",  tempoCount: 1 },
+    { formula: "6x8",  tempoCount: 2 },
+    { formula: "9x8",  tempoCount: 3 },
+    { formula: "12x8", tempoCount: 4 }
+  ],
+
+  calculate: function(bpm, timeSignature, barCount) {
+    beatInSeconds = 60 / bpm
+    totalTempoCount = timeSignature.tempoCount * barCount
+    return Math.round(beatInSeconds * totalTempoCount);
+  }
+}
+
+module.exports = Minutagem
