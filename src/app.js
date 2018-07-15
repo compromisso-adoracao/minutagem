470f677da15ae1d6ffbe92450d058c81bf893613
Vinicius Brasil <marcosbrasilaraujo@gmail.com>
2018-07-15 09:58:47 -0300

    Move minutagem.js to a module

--- src/app.js
+++ src/app.js
@@ -10,25 +10,6 @@ String.prototype.toHHMMSS = function () {
   return hours+':'+minutes+':'+seconds;
 }
 
-const Minutagem = {
-  timeSignatures: [
-    { formula: "2x4",  tempoCount: 2 },
-    { formula: "3x4",  tempoCount: 3 },
-    { formula: "4x4",  tempoCount: 4 },
-    { formula: "6x4",  tempoCount: 6 },
-    { formula: "3x8",  tempoCount: 1 },
-    { formula: "6x8",  tempoCount: 2 },
-    { formula: "9x8",  tempoCount: 3 },
-    { formula: "12x8", tempoCount: 4 }
-  ],
-
-  calculate: function(bpm, timeSignature, barCount) {
-    beatInSeconds = 60 / bpm
-    totalTempoCount = timeSignature.tempoCount * barCount
-    return Math.round(beatInSeconds * totalTempoCount);
-  }
-}
-
 const populateTimeSignatureOptions = function() {
   const selectElement = document.querySelector("#signature-formula")
   Minutagem.timeSignatures.forEach((timeSignature) => {
@@ -82,6 +63,7 @@ window.onload = function() {
   document.
     querySelector("#calculate-button").
     addEventListener("click", function() {
+      controlResult(hide = true)
       controlValidationError(hide = true)
 
       const resultado = calculateMinutagem();
